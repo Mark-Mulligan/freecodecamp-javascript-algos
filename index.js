317a6b341d20257d32e99926b0c38fa7b53689e9
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-04-20 16:51:37 -0500

    largest numbers in arrays

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function factorialize(num) {
   return sum;
 }
 
-//Find the Longest Word in a StringPassed
+//Find the Longest Word in a String
 function findLongestWordLength(str) {
   const words = str.split(' ');
   let longestWord = words[0].length;
@@ -32,7 +32,17 @@ function findLongestWordLength(str) {
   return longestWord;
 }
 
-console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+//Return Largest Numbers in Arrays
+function largestOfFour(arr) {
+  const largestNumbers = [];
 
+  arr.forEach(subArr => {
+    largestNumbers.push(Math.max(...subArr));
+  })
+  
+  return largestNumbers;
+}
+
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
 
 
