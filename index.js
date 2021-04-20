d6a142f3234cdccfab6763f75a1413187fbb73d7
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-04-20 16:38:53 -0500

    convert celsius to fahrenheit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello');
+/* Basic Algorithmn Scripting */
+
+//Convert Celsius to Fahrenheit
+function convertToF(celsius) {
+  return celsius * (9/5) + 32;
+}
+
+console.log(convertToF(30));
