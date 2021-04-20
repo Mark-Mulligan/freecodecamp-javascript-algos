2abcfca71048ca98d0e21b81906842ff69beb0a5
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-04-20 17:05:23 -0500

    confirm ending in a string

--- index.js
+++ index.js
@@ -43,6 +43,12 @@ function largestOfFour(arr) {
   return largestNumbers;
 }
 
-console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+//Confirm the Ending In String
+function confirmEnding(str, target) {
+  const ending = str.slice(str.length - target.length, str.length);
+  return ending === target;
+}
+
+console.log(confirmEnding("Bastian", "xn"));
 
 
