c81f7d5deb151e34d6e4628c6256c5d89fbbb93a
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-04-20 23:22:47 -0500

    Mutations Problem

--- index.js
+++ index.js
@@ -132,5 +132,17 @@ function getIndexToIns(arr, num) {
   return sortedArr.length;
 }
 
+//Mutations
+function mutation(arr) {
+  const firstWord = arr[0].toLowerCase();
+  const secondWord = arr[1].toLowerCase().split('');
 
+  for (let i = 0; i < secondWord.length; i++) {
+    if (!firstWord.includes(secondWord[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
