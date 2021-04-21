b8660e5a4e99383d4e1de79e3efd16a844b53852
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-04-21 08:26:41 -0500

    sum all

--- index.js
+++ index.js
@@ -168,3 +168,19 @@ function chunkArrayInGroups(arr, size) {
   return resultArr;
 }
 
+/* ----- Intermediate Algorithm Scripting ----- */
+
+//Sum All Numbers in a Range
+function sumAll(arr) {
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let sum = 0;
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
+
+console.log(sumAll([1, 4]));
