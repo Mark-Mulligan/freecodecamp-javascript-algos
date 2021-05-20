17e769e2a65b891b00f2644dd6d36fa75126fd60
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-05-19 20:08:47 -0500

    everyting be true

--- index.js
+++ index.js
@@ -401,3 +401,25 @@ function binaryAgent(str) {
     .join("");
 }
 
+/* Everything Be True */
+function truthCheck(collection, pre) {
+  for (let i = 0; i < collection.length; i++) {
+    if (!collection[i][pre]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(
+  truthCheck(
+    [
+      { user: "Tinky-Winky", sex: "male" },
+      { user: "Dipsy", sex: "male" },
+      { user: "Laa-Laa", sex: "female" },
+      { user: "Po", sex: "female" },
+    ],
+    "sex"
+  )
+);
